b96f4daefa20f074e473e2bcb404a1fb38454d67
wangxun <wangxun@cyai.com>
2022-08-16 11:42:33 +0800

    feat:Day06-与范本共舞

--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -1,14 +1,25 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { apply, mergeWith, Rule, SchematicContext, strings, template, Tree, url } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function helloWorld(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    //产生一个档案抿成为使用者所输入的 --name 参数的值，没输入则为'hello'的档案
-    //档案内容为'world'
-    tree.create(_options.name || 'hello','world');
+  return (_tree: Tree, _context: SchematicContext) => {
 
-    return tree;
+    const sourceTemplates = url('./files');
+
+    const sourceParametrizedTemplates = apply(sourceTemplates,[
+      template({
+        ..._options,
+        ...strings, //将这些函式加到规则里，范本语法才能正常运作
+        addExclamation
+      })
+    ]);
+
+    return mergeWith(sourceParametrizedTemplates);
   };
+}
+
+function addExclamation(_value:string):string{
+  return _value = '!';
 }
