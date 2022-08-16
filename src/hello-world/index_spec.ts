740cdd74562e3fb4118e08532d2cacb3a086b79c
wangxun <wangxun@cyai.com>
2022-08-16 10:34:00 +0800

    feat:Day04为你的Schematics撰写测试程式

--- src/hello-world/index_spec.ts
+++ src/hello-world/index_spec.ts
@@ -5,12 +5,22 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('hello-world', () => {
-  it('works', async () => {
+
+  const expectResult = async (fileName?:string) => {
+    const fullFileName = `/${fileName || 'hello'}`;
+    const params = fileName ? {name: fileName} : {};
     const runner = new SchematicTestRunner('schematics', collectionPath);
     const tree = await runner
-      .runSchematicAsync('hello-world', {}, Tree.empty())
-      .toPromise();
+    .runSchematicAsync('hello-world', params, Tree.empty())
+    .toPromise();
+    expect(tree.files).toContain(fullFileName);
+    expect(tree.readContent(fullFileName)).toEqual('world');
+  } 
 
-    expect(tree.files).toEqual([]);
+  it('使用者没给档案名，则档案名为\'hello\'', async () => {
+    expectResult();
+  });
+  it('使用者有给档案名，则档案名为使用者的档案名', async () => {
+    expectResult('Leo');
   });
 });
